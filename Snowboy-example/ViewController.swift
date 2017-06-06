b18ccacf66164eff8656fdfe17ed8d317a499443
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-06-07 04:20:56 +0530

    Initial Commit

---
+++ Snowboy-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snowboy-example
+//
+//  Created by Chashmeet Singh on 2017-06-07.
+//  Copyright © 2017 Chashmeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
